b0d4a225d951f358717e039b3fd8912e487881ad
Marshilla Brahma <marshilla.brahma@gmail.com>
2019-04-16 13:20:20 -0400

    Added google dependency and updated String and PowerSet

--- src/main/java/rocks/zipcode/io/collections/PowerSet.java
+++ src/main/java/rocks/zipcode/io/collections/PowerSet.java
@@ -1,6 +1,9 @@
 package rocks.zipcode.io.collections;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.google.common.collect.Sets;
 
 /**
  * @author leon on 10/01/2019.
@@ -16,17 +19,29 @@ public class PowerSet<TypeOfSet> {
      * @return the powerset of `originalSet`
      */
     public Set<Set<TypeOfSet>> permute() {
-        Set<Set<TypeOfSet>> megaSet = new HashSet<>();
-        List<TypeOfSet> list = new ArrayList<>(originalSet);
+//        Set<Set<TypeOfSet>> megaSet = new HashSet<>();
+//        List<TypeOfSet> list = new ArrayList<>(originalSet);
+//
+//        for(int i = 0 ; i < list.size(); i++){
+//            for(int j = i; j < list.size(); j++){
+//                megaSet.add(new HashSet<>(list.subList(i,j+1)));
+//
+//                if(j+1 < list.size()){
+//                    megaSet.add(new HashSet<>(Arrays.asList(list.get(i),list.get(j+1))));
+//                }
+//
+//                if(j > i && j+1 < list.size()){
+//                    List<TypeOfSet> firstList = list.subList(i,j-1);
+//                    firstList.addAll(list.subList(j+1,list.size()-1));
+//                    megaSet.add(new HashSet<>(firstList));
+//                }
+//            }
+//        }
+//        //megaSet.forEach(item -> System.out.println(item));
+//        return megaSet;
 
-        for(int i = 0 ; i < list.size(); i++){
-            for(int j = i; j < list.size(); j++){
-                megaSet.add(new HashSet<>(list.subList(i,j+1)));
-            }
-        }
-        return megaSet;
+        return Sets.powerSet(originalSet);
     }
-
     /**
      * @return `originalSet`
      */
--- src/main/java/rocks/zipcode/io/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/fundamentals/StringUtils.java
@@ -1,7 +1,12 @@
 package rocks.zipcode.io.fundamentals;
 
 
+import rocks.zipcode.io.arrays.ArrayUtils;
+import rocks.zipcode.io.collections.PowerSet;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 /**
  * @author leon on 10/01/2019.
@@ -12,13 +17,23 @@ public class StringUtils {
      * @return collection containing all permutations of casing of this string
      */
     public static Collection<String> getAllCasings(String string) {
+        Collection<String> result = new ArrayList<>();
         // get length of string
+        Integer length = string.length();
         // get range of length
+        Integer[] range = ArrayUtils.getRange(0,length-1);
         // get power-set of range
-
+        PowerSet<Integer> powerSet = new PowerSet<>(range);
+        Set<Set<Integer>> powerPowerSet = powerSet.permute();
+        powerPowerSet.forEach(item -> System.out.println(item));
         // for every set in power-set
+        for(Set<Integer> set : powerPowerSet){
             // uppercase indices of string using set
-        return null;
+            result.add(upperCaseIndices(string,set.toArray(new Integer[0])));
+        }
+        result.add(string);
+        result.forEach(item -> System.out.println(item));
+        return result;
     }
 
     /**
@@ -27,7 +42,21 @@ public class StringUtils {
      * @return near-identical string whose characters at specified indices are capitalized
      */
     public static String upperCaseIndices(String string, Integer... indices) {
-        return null;
+        String result = string;
+        for (Integer i : indices) {
+            Character replacement = Character.toUpperCase(string.charAt(i));
+            result = replaceAtIndex(result,replacement,i);
+        }
+        return result;
+    }
+
+    public static String lowerCaseIndices(String string, Integer... indices) {
+        String result = string;
+        for (Integer i : indices) {
+            Character replacement = Character.toLowerCase(string.charAt(i));
+            result = replaceAtIndex(result,replacement,i);
+        }
+        return result;
     }
 
     /**
@@ -37,7 +66,7 @@ public class StringUtils {
      * @return near-identical string with `valueToBeInserted` inserted at `index`
      */
     public static String insertAtIndex(String stringToBeManipulated, String valueToBeInserted, Integer index) {
-        return null;
+        return stringToBeManipulated.substring(0,index)+valueToBeInserted+stringToBeManipulated.substring(index);
     }
 
     /**
@@ -47,6 +76,6 @@ public class StringUtils {
      * @return near-identical string with character at `index` replaced with `replacementValue`
      */
     public static String replaceAtIndex(String stringToBeManipulated, Character replacementValue, Integer index) {
-        return null;
+        return stringToBeManipulated.substring(0,index)+replacementValue+stringToBeManipulated.substring(index+1);
     }
 }
