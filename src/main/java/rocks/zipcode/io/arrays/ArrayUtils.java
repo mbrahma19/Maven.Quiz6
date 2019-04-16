15eead8ef0655eebf351e71c515fc994b46f7959
Marshilla Brahma <marshilla.brahma@gmail.com>
2019-04-16 10:16:05 -0400

    Completed Srray and BasicString Utils

--- src/main/java/rocks/zipcode/io/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/io/arrays/ArrayUtils.java
@@ -1,5 +1,9 @@
 package rocks.zipcode.io.arrays;
 
+import rocks.zipcode.io.fundamentals.BasicStringUtils;
+
+import java.util.stream.IntStream;
+
 /**
  * @author leon on 24/01/2019.
  */
@@ -10,7 +14,7 @@ public class ArrayUtils {
      * @return array of all integers between `start` and `stop`
      */
     public static Integer[] getRange(Integer start, Integer end) {
-        return null;
+        return IntStream.rangeClosed(start,end).boxed().toArray(Integer[]::new);
     }
 
     /**
@@ -18,7 +22,7 @@ public class ArrayUtils {
      * @return array with identical contents in primitive-array form
      */
     public static char[] unbox(Character[] array) {
-        return null;
+        return BasicStringUtils.getString(array).toCharArray();
     }
 
     /**
@@ -26,6 +30,10 @@ public class ArrayUtils {
      * @return array with identical contents in nonprimitive-array form
      */
     public static Character[] box(char[] array) {
-        return null;
+        Character[] newArray = new Character[array.length];
+        for(int i = 0 ; i < array.length; i++){
+            newArray[i] = array[i];
+        }
+        return newArray;
     }
 }
--- src/main/java/rocks/zipcode/io/fundamentals/BasicStringUtils.java
+++ src/main/java/rocks/zipcode/io/fundamentals/BasicStringUtils.java
@@ -1,12 +1,17 @@
 package rocks.zipcode.io.fundamentals;
 
+import rocks.zipcode.io.arrays.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class BasicStringUtils {
     /**
      * @param chars - characters used to instantiate a new string object
      * @return new String which wraps the arguments passed in
      */
     public static String getString(char[] chars) {
-        return null;
+        return String.valueOf(chars);
     }
 
     /**
@@ -14,7 +19,9 @@ public class BasicStringUtils {
      * @return new String which wraps the arguments passed in
      */
     public static String getString(Character[] chars) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(chars).forEach(s -> sb.append(s));
+        return sb.toString();
     }
 
     /**
@@ -22,7 +29,7 @@ public class BasicStringUtils {
      * @return identical string with lowercase and uppercase vowels removed
      */
     public static String removeAllVowels(String string) {
-        return null;
+        return removeSpecifiedCharacters(string, "aeiou");
     }
 
     /**
@@ -31,6 +38,12 @@ public class BasicStringUtils {
      * @return
      */
     public static String removeSpecifiedCharacters(String string, String charactersToRemove) {
-        return null;
+        Character[] charArray = ArrayUtils.box(string.toCharArray());
+        Character[] newArray = Stream.of(charArray).filter(c -> !matchesCharactertoRemove(c,charactersToRemove)).toArray(Character[]::new);
+        return getString(newArray);
+    }
+
+    public static Boolean matchesCharactertoRemove(char letter, String charactersToRemove){
+        return String.valueOf(letter).toLowerCase().matches("[" +charactersToRemove.toLowerCase()+"]+");
     }
 }
